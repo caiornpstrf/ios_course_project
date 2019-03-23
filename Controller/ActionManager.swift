2c0188785dbee6d0bdb3828290f134ea6e0ee85a
Reis, Caio <caio.reis@lexisnexisrisk.com>
2019-03-22 22:54:37 -0300

    Added image, alert buttons, url calls

---
+++ Controller/ActionManager.swift
@@ -0,0 +1,13 @@
+//
+//  ActionManager.swift
+//  Native Mood App?
+//
+//  Created by Caio Reis on 22/03/19.
+//  Copyright © 2019 Caio Reis. All rights reserved.
+//
+
+import UIKit
+
+class ActionManager: NSObject {
+
+}
